e208d625ae61776e66453f91975b0d5a9e3af1f4
amitg01 <amitg.ghy@gmail.com>
2022-05-21 16:57:59 +0530

    scoped backend routes and also modified routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope :api, defaults: { format: :json } do
+    devise_for :users, path_names: { sign_in: :login, sign_out: :logout }
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
