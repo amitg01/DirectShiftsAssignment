175aa4ececa3d9b35b0a30acde05b6f05048aefe
amitg01 <amitg.ghy@gmail.com>
2022-05-24 12:27:20 +0530

    Modified Dashboard component

--- app/javascript/src/components/dashboard/index.jsx
+++ app/javascript/src/components/dashboard/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Navbar from "./Navbar";
 
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 const Dashboard = () => {
   return (
     <>
       <Navbar />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <Typography variant="h3" component="h2">
+            Welcome To DirectShifts
+          </Typography>
+        </Grid>
+      </Grid>
     </>
   );
 };
